3934100301d169def9e33bd0fee97e4b17ed933c
auxilum <haribomedvjedic@gmail.com>
2023-06-27 15:03:20 +0200

    final state

--- whack-a-mole/mole.js
+++ whack-a-mole/mole.js
@@ -21,7 +21,7 @@ function popUpRandomMole() {
   clickable = true;
 
   // UNCOMMENT THIS LINE OF CODE WHEN DIRECTED
-  // moleHead.classList.remove('wgs__mole-head--hidden', 'wgs__mole-head--whacked');
+  moleHead.classList.remove('wgs__mole-head--hidden', 'wgs__mole-head--whacked');
 
   molesLeft -= 1;
   document.querySelector('.sb__moles').innerHTML = molesLeft;
@@ -52,7 +52,7 @@ window.addEventListener('DOMContentLoaded', () => {
       hideMole(event.target);
 
       // UNCOMMENT THIS LINE OF CODE WHEN DIRECTED
-      // event.target.classList.add('wgs__mole-head--hidden');
+      event.target.classList.add('wgs__mole-head--hidden');
 
       // UNCOMMENT THIS LINE OF CODE WHEN DIRECTED FOR THE BONUS
       // event.target.classList.add('wgs__mole-head--whacked');
@@ -60,13 +60,13 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-window.addEventListener('DOMContentLoaded', () => {
+  //window.addEventListener('DOMContentLoaded', () => {
 
-    setInterval(() => {
-      const moleHeads = document.querySelectorAll('.wgs__mole-head');
-      for (let moleHead of moleHeads) {
-        moleHead.classList.toggle('wgs__mole-head--hidden');
-      }
-    }, 1000);
+  //setInterval(() => {
+  // const moleHeads = document.querySelectorAll('.wgs__mole-head');
+  // for (let moleHead of moleHeads) {
+  //  moleHead.classList.toggle('wgs__mole-head--hidden');
+  //  }
+  // }, 1000);
   
-  });
+  // });
